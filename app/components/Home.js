552301330e525a57085e559fc169817cbbdae665
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-14 14:17:29 +0100

    theme usage

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,21 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 import theme from '../config/theme';
 
+import TableMarket from './TableMarket';
+
 const Title = styled.h1`
   font-family: ${theme.fontFamily}, sans-serif;
+  -webkit-font-smoothing: antialiased;
   color: ${theme.primaryColor};
   font-size: ${theme.titleSize}px;
+  line-height: ${theme.lineHeight}rem;
+  letter-spacing: ${theme.letterSpacing}rem;
   text-align: center;
-  padding-top: 2%;
 `;
 
 const Container = styled.div`
+  padding-top: 2%;
 `;
 
 const App = () => {
   return (
     <Container>
-      <Title>Crypto folio</Title>
+      <Title>Crypto Folio</Title>
+      <TableMarket />
     </Container>
   );
 };
--- app/config/theme.js
+++ app/config/theme.js
@@ -1,8 +1,16 @@
 const Default = {
   primaryColor: '#ffffff',
+  borderColorTable: '#ffffff',
   backgroundColor: '#000000',
+  backgroundColorTable: '#1c1c1c',
   fontFamily: 'Roboto',
-  titleSize: 20,
+  fontWeight: 300,
+  lineHeight: 2,
+  letterSpacing: 0.01,
+  titleSize: 60,
+  coinColor: 'white',
+  changePositive: '#00c853',
+  changeNegative: '#f44336',
 };
 
 export default Default;
