402cfd30643fa457786fab2ead25d017ea14c3b9
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-14 19:53:39 +0100

    fetch for each coins

--- app/infrastructure/searchInformation.js
+++ app/infrastructure/searchInformation.js
@@ -1,10 +1,14 @@
-const fetch = require('fetch');
-
-function fetchInformation(coin) {
-  return fetch(coin.information, { method: 'GET' })
+function fetchInformationByCoin(coin) {
+  return fetch(coin.link, { method: 'GET' })
     .then(res => res.json())
-    .then((res) => { return res[0]; })
+    .then((res) => {
+      return Object.assign({}, coin, res[0]);
+    })
     .catch(({ code: 'ERROR_FETCH', message: `Error for coin ${coin.symbol} link : ${coin.information}` }));
+}
+
+function fetchInformation(coins) {
+  return Promise.all(coins.map(fetchInformationByCoin));
 }
 
 export default fetchInformation;
