f8a69d61871cbe82ac48c11240561e19e004fc79
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-14 18:00:21 +0100

    use trend for a fashion component

---
+++ app/components/Trend.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TrendComp from 'react-trend';
+
+const Trend = () => (
+  <TrendComp
+    smooth
+    width={300}
+    autoDraw
+    autoDrawDuration={3000}
+    autoDrawEasing="ease-out"
+    data={[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]}
+    gradient={['red', 'orange', 'yellow']}
+    radius={10}
+    strokeWidth={2.8}
+    strokeLinecap="round"
+  />
+);
+
+export default Trend;
