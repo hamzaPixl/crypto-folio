8d0d7e12d9a4e6662b1a124b51db0f82f0755d9a
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-14 18:00:05 +0100

    modify wallet and adapt logo

--- app/config/wallet.js
+++ app/config/wallet.js
@@ -1,42 +1,62 @@
-/* eslint react/react-in-jsx-scope: 0 */
-import * as Icon from 'react-cryptocoins';
+import React from 'react';
+
+import ADA from '../assets/icons/coins/ada.svg';
+import XLM from '../assets/icons/coins/xlm.svg';
+import XRP from '../assets/icons/coins/xrp.svg';
+import ETH from '../assets/icons/coins/eth.svg';
+import CND from '../assets/icons/coins/cnd.png';
+import TRX from '../assets/icons/coins/trx.svg';
+import XVG from '../assets/icons/coins/xvg.svg';
 
 const Wallet = [
   {
     hodl: 0.30223045,
     name: 'Ethereum',
     symbol: 'ETH',
-    component: <Icon.Eth />,
+    icon: <img src={ETH} alt="ETH" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/ethereum/',
   },
   {
     hodl: 100.649,
     name: 'Ripple',
     symbol: 'XRP',
-    component: <Icon.Xrp />,
+    icon: <img src={XRP} alt="XRP" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/ripple/',
   },
   {
     hodl: 74.915,
     name: 'Stellar',
     symbol: 'XLM',
-    component: <Icon.Xlm />,
+    icon: <img src={XLM} alt="XLM" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/stellar/',
   },
   {
     hodl: 291.104,
     name: 'TRON',
     symbol: 'TRX',
-    component: <Icon.Trx />,
+    icon: <img src={TRX} alt="TRX" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/tron/',
   },
   {
     hodl: 300.24918,
     name: 'Verge',
     symbol: 'XVG',
-    component: <Icon.Xvg />,
+    icon: <img src={XVG} alt="XVG" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/verge/',
   },
   {
     hodl: 24.975,
     name: 'Cardano',
     symbol: 'ADA',
-    component: <Icon.Ada />,
+    icon: <img src={ADA} alt="ADA" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/cardano/',
+  },
+  {
+    hodl: 70.846,
+    name: 'Cindicator',
+    symbol: 'CND',
+    icon: <img width="30px" src={CND} alt="CND" />,
+    information: 'https://api.coinmarketcap.com/v1/ticker/cindicator/',
   },
 
 ];
