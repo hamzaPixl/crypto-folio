ed9b55fcacfb61082b0f0e77562e946c3f42d295
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-14 17:59:51 +0100

    change theme

--- app/config/theme.js
+++ app/config/theme.js
@@ -2,7 +2,7 @@ const Default = {
   primaryColor: '#ffffff',
   borderColorTable: '#ffffff',
   backgroundColor: '#000000',
-  backgroundColorTable: '#1c1c1c',
+  backgroundColorTable: '#263238',
   fontFamily: 'Roboto',
   fontWeight: 300,
   lineHeight: 2,
@@ -11,6 +11,7 @@ const Default = {
   coinColor: 'white',
   changePositive: '#00c853',
   changeNegative: '#f44336',
+  progressCircle: '#ff4081',
 };
 
 export default Default;
