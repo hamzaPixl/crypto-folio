a1ebc1d9be0f7356d79ded13354e98a5ff41cb1f
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-19 16:26:21 +0100

    create the table portfolio

---
+++ app/components/TablePortfolio.js
@@ -0,0 +1,84 @@
+/* eslint react/no-did-mount-set-state: 0 */
+/* eslint react/forbid-prop-types: 0 */
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../config/theme';
+
+import FormattedCurrency from './FormattedCurrency';
+import FormattedCoin from './FormattedCoin';
+import ProgressBar from './ProgressBar';
+
+const Container = styled.div`
+  padding-top: 2%;
+  display: flex;
+  justify-content: center;
+`;
+
+const TableContainer = styled.table`
+  width: 300px;
+  text-align: left;
+  border-spacing: 5px;
+  border-radius: 4px;
+  font-family: ${theme.fontFamily}, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  color: ${theme.primaryColor};
+  font-size: 15px;
+  line-height: ${theme.lineHeight}rem;
+  letter-spacing: ${theme.letterSpacing}rem;
+  margin-bottom: 2rem;
+  background-color: ${theme.backgroundColorTable};
+  td {
+    padding-right: 15px;
+  }
+`;
+
+const CoinContainer = styled.div`
+  display: flex;
+  padding-left: 10px;
+`;
+
+function TablePortfolio(props) {
+  const totalPrice = props.coins.reduce((a, b) => (a + b.totalPrice), 0);
+  return (
+    <Container>
+      <TableContainer>
+        <tbody>
+          {
+            props.coins.map((coin) => {
+              const percent = Math.round((coin.totalPrice / totalPrice) * 100);
+              return (
+                <tr key={coin.name}>
+                  <td>
+                    <CoinContainer>
+                      {coin.symbol}
+                    </CoinContainer>
+                  </td>
+                  <td>
+                    <FormattedCurrency value={coin.totalPrice} />
+                  </td>
+                  <td>
+                    <FormattedCoin value={coin.totalBTC} />
+                  </td>
+                  <td>
+                    <ProgressBar percent={percent} />
+                  </td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </TableContainer>
+    </Container>
+  );
+}
+
+TablePortfolio.propTypes = {
+  coins: PropTypes.array,
+};
+
+TablePortfolio.defaultProps = {
+  coins: [],
+};
+
+export default TablePortfolio;
