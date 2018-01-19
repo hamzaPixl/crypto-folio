22ec2a0ada72f12ea63c0e7e5b6f9ff45bfef599
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-19 16:25:21 +0100

    add some informations

--- app/infrastructure/searchInformation.js
+++ app/infrastructure/searchInformation.js
@@ -2,7 +2,10 @@ function fetchInformationByCoin(coin) {
   return fetch(coin.link, { method: 'GET' })
     .then(res => res.json())
     .then((res) => {
-      return Object.assign({}, coin, res[0]);
+      return Object.assign({}, coin, res[0], {
+        totalPrice: res[0].price_usd * coin.hodl,
+        totalBTC: res[0].price_btc * coin.hodl,
+      });
     })
     .catch(({ code: 'ERROR_FETCH', message: `Error for coin ${coin.symbol} link : ${coin.information}` }));
 }
