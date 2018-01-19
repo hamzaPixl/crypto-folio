bbe81c6cde8c28cc9f0f401f6519d365fbe87791
Hamza Mounir <hamza.pixelle@gmail.com>
2018-01-19 20:50:31 +0100

    add resume portfolio

---
+++ app/components/ResumePortfolio.js
@@ -0,0 +1,42 @@
+/* eslint react/style-prop-object: 0 */
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+
+import FormattedCurrency from './FormattedCurrency';
+import ProgressCircle from './ProgressCircle';
+import theme from '../config/theme';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${theme.primaryColor};
+  flex-direction: column;
+  font-family: ${theme.fontFamily}, sans-serif;
+`;
+
+const PriceUSD = styled.div`
+  padding-top: 30px;
+  font-size: 40px;
+`;
+
+function ResumePortfolio(props) {
+  return (
+    <Container>
+      <ProgressCircle percent={100} />
+      <PriceUSD>
+        <FormattedCurrency value={props.totalPrice} />
+      </PriceUSD>
+    </Container>
+  );
+}
+
+ResumePortfolio.propTypes = {
+  totalPrice: PropTypes.number,
+};
+
+ResumePortfolio.defaultProps = {
+  totalPrice: 0,
+};
+
+export default ResumePortfolio;
