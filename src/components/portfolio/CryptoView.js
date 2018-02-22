4c430ea70d1d0a0aa670a5bdb8929379a790e756
Hamza Mounir <hamza.pixelle@gmail.com>
2018-02-22 16:31:36 +0100

    add some configuration

--- src/components/portfolio/CryptoView.js
+++ src/components/portfolio/CryptoView.js
@@ -1,16 +1,22 @@
 /* eslint react/style-prop-object: 0 */
+/* eslint prefer-destructuring: 0 */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import { FormattedPercentage, FormattedCurrency, FormattedCoin } from '../formatted';
 import theme from '../../utils/theme';
 
-const Container = styled.div`
-`;
+const URL_API = 'https://api.coinmarketcap.com/v1/ticker/';
+const URL_ICON = 'https://coincodex.com/en/resources/images/admin/coins/';
+
+const Container = styled.div``;
 
 const Name = styled.div``;
 
-const Symbol = styled.div``;
+const Symbol = styled.div`
+  padding-right: 5px;
+`;
 
 const Icon = styled.div``;
 
@@ -20,22 +26,36 @@ const PriceUSD = styled.div``;
 
 const Percent = styled.div``;
 
-const SymbolContainer = styled.div``;
-
-const SearchContainer = styled.div``;
-
-const Search = styled.input``;
+const SymbolContainer = styled.div`
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
 
 const PriceContainer = styled.div``;
+
+const SearchContainer = styled.div`
+  padding-top: 10%;
+  padding-bottom: 10%;
+`;
+
+const Search = styled.input`
+  text-align:center;
+  color: ${props => theme[props.theme].dotColor};
+  font-size: 25px;
+  border: 2px solid ${props => theme[props.theme].dotColor};
+  border-radius: 3px;
+`;
 
 class CryptoView extends Component {
   constructor(props) {
     super(props);
     this.state = {
       coin: {
-        name: 'TRON',
-        symbol: 'TRX',
-        icon: 'T',
+        name: 'tron',
+        symbol: 'trx',
+        icon: 'https://coincodex.com/en/resources/images/admin/coins/tron.png',
         price_btc: '0.0823515',
         price_usd: '949.314',
         percent_change_24h: '-2',
@@ -43,11 +63,15 @@ class CryptoView extends Component {
     };
   }
 
-  fetchInformations(event, data) {
-    const url = `https://api.coinmarketcap.com/v1/ticker/${data}`;
+  handleSearch(event, data) {
+    const url = `${URL_API}${data}`;
     fetch(url, { method: 'GET' })
+      .then(res => res.json())
       .then((res) => {
-
+        const coinFound = Array.isArray(res);
+        if (coinFound) {
+          this.state.coin = Object.assign({}, res[0], { icon: `${URL_ICON}${res[0].name}` });
+        }
       });
   }
 
@@ -57,10 +81,12 @@ class CryptoView extends Component {
     return (
       <Container>
         <SearchContainer>
-          <Search />
+          <Search
+            theme={this.props.theme}
+            placeholder="Search"
+            type="text"
+          />
         </SearchContainer>
-
-        <Icon>{this.state.coin.icon}</Icon>
 
         <SymbolContainer>
           <Symbol>{this.state.coin.symbol}</Symbol>
@@ -68,9 +94,24 @@ class CryptoView extends Component {
         </SymbolContainer>
 
         <PriceContainer>
-          <PriceBTC>{this.state.coin.price_btc}</PriceBTC>
-          <PriceUSD>{this.state.coin.price_usd}</PriceUSD>
-          <Percent>{change24}</Percent>
+          <PriceBTC>
+            <FormattedCoin
+              symbol="฿"
+              value={this.state.coin.price_btc}
+            />
+          </PriceBTC>
+          <PriceUSD>
+            <FormattedCurrency
+              value={this.state.coin.price_usd}
+              class={percentageClass}
+            />
+          </PriceUSD>
+          <Percent>
+            <FormattedPercentage
+              value={change24}
+              class={percentageClass}
+            />
+          </Percent>
         </PriceContainer>
       </Container>
     );
