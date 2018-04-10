64eba81f59063bcc4f8219463c9ab462c24f534c
Hamza Mounir <hamza.pixelle@gmail.com>
2018-04-10 11:02:25 +0200

    create rss feed search news

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,5 +1,6 @@
 import searchInformation from './searchInformation';
 import searchCoin from './searchCoin';
 import listCoins from './listCoins';
+import searchNews from './searchNews';
 
-export { searchCoin, searchInformation, listCoins };
+export { searchNews, searchCoin, searchInformation, listCoins };
---
+++ src/infrastructure/searchNews.js
@@ -0,0 +1,11 @@
+const Parser = require('rss-parser');
+
+const parser = new Parser();
+
+async function fetchNews() {
+  const urlRss = 'https://cointelegraph.com/rss';
+  const feed = await parser.parseURL(urlRss);
+  return feed.items.map(i => i.title);
+}
+
+export default fetchNews;
